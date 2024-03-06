226932e07102eb5a6991224263524d9c98ddc317
Arkaprabha Das <arkaprabhadas@Arkaprabhas-MacBook-Air.local>
2024-03-07 01:54:27 +0530

    third commit

--- db/db.js
+++ db/db.js
@@ -1,16 +1,62 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://arkaprabha31:Password1234!@todolist.vp7s3ci.mongodb.net/User');
+// // Define schemas
+
+// const UserSchema = new mongoose.Schema({
+//     // Schema definition here
+//     username: String,
+//     password: String
+// });
+
+// const User = mongoose.model('User', UserSchema);
+
+// module.exports = {
+//     User
+// }
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://arkaprabha31:Password1234!@mernstack2024.zghjkgu.mongodb.net/TodoList')
 
-mongoose.connect('mongodb+srv://arkaprabha31:Password1234!@todolist.vp7s3ci.mongodb.net/User');
-// Define schemas
+const TodoSchema = new mongoose.Schema({task: String})
+const Todo = mongoose.model('Todo', TodoSchema);
 
-const UserSchema = new mongoose.Schema({
-    // Schema definition here
-    username: String,
-    password: String
-});
+    const UserSchema = new mongoose.Schema({
+        text:String
+    });
+    const User = mongoose.model('User', UserSchema);
 
-const User = mongoose.model('User', UserSchema);
 
-module.exports = {
-    User
-}
+app.get('/signup', async(req,res)=>{
+    try{
+        const todos = await Todo.find();
+        res.send(todos);
+    }catch(err){
+        res.send(err);
+    }
+})
+
+app.post('/signup', async(req,res)=>{
+    new User({
+                message: req.body.message
+            }).save();
+            res.send({
+                        message: req.body
+                    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server running on PORT 3000')
+})
+
+
+
--- db/index.js
+++ db/index.js
@@ -1,23 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const {User} = require('./db');
-const PORT = 3000;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const {User} = require('./db');
+// const PORT = 3000;
 
-// Middleware for parsing request bodies
-app.use(bodyParser.json());
+// // Middleware for parsing request bodies
+// app.use(bodyParser.json());
 
-app.post('/signup', (req, res) => {
-    // Implement user signup logic
-    new User({
-        username: req.body.username,
-        password: req.body.password
-    }).save();
-    res.json({
-        message: 'User created successfully'
-    })
-});
+// app.post('/signup', (req, res) => {
+//     // Implement user signup logic
+//     new User({
+//         username: req.body.username,
+//         password: req.body.password
+//     }).save();
+//     res.json({
+//         message: 'User created successfully'
+//     })
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const {Todo} = require('./db');
+// const PORT = 3000;
+
+// app.get('/signup', async (req,res)=>{
+//     const todos = Todo.find();
+//     res.send(todos);
+// })
+
+// app.listen(PORT, ()=>{
+//     console.log("Server running onn port 3000")
+// })
